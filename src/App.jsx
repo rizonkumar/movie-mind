61b147d8d05b53e35ff872421dcdedddb27267ba
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-08-03 17:34:34 +0530

    Fix: Code Format

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './components/Home'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className='bg-[#1F1E24] w-screen h-screen flex'>
+    <div className="bg-[#1F1E24] w-screen h-screen flex">
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
