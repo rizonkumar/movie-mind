315a498730aaf8749fe13ddc5f5d6c96d442a97c
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-08-03 08:41:01 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div className='bg-[#1F1E24] w-screen h-screen flex'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
