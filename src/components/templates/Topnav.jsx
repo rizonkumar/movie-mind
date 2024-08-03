e29f5ba346fd4497be6fcf5f26a4a5dbce97a59f
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-08-03 09:28:48 +0530

    Feat: Search API Integration & Rendering the movies in the UI

--- src/components/templates/Topnav.jsx
+++ src/components/templates/Topnav.jsx
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
+import axios from "../../utils/axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Topnav = () => {
   const [query, setQuery] = useState("");
+  const [search, setSearch] = useState([]);
+
+  const getSearch = async () => {
+    try {
+      const { data } = await axios.get(`/search/multi?query=${query}`);
+      console.log("Data", data);
+      setSearch(data.results);
+    } catch (error) {
+      console.log("Error : ", error);
+    }
+  };
+
+  useEffect(() => {
+    getSearch();
+  }, [query]);
+
   console.log("Query", query);
 
   return (
@@ -17,18 +34,39 @@ const Topnav = () => {
           placeholder="Search anything..."
         />
         {query.length > 0 && (
-          <i onClick={() => setQuery("")} className="absolute right-3 text-zinc-400 text-xl ri-close-fill cursor-pointer"></i>
+          <i
+            onClick={() => setQuery("")}
+            className="absolute right-3 text-zinc-400 text-xl ri-close-fill cursor-pointer"
+          ></i>
         )}
 
-        <div className="absolute w-full max-h-[60vh] top-[120%] left-0 bg-[#2C2C2C] overflow-auto rounded-lg shadow-lg">
-          <Link className="hover:bg-[#3A3A3A] duration-300 text-zinc-300 font-semibold w-full p-4 flex items-center border-b border-zinc-600">
-            <img src="" alt="" className="w-10 h-10 mr-3 bg-zinc-600 rounded" />
-            <span>Hellloooooooo</span>
-          </Link>
-        </div>
+        {search.length > 0 && (
+          <div className="absolute w-full max-h-[60vh] top-[120%] left-0 bg-[#2C2C2C] overflow-auto rounded-lg shadow-lg">
+            {search.map((item, index) => (
+              <Link
+                key={index}
+                to={`/${item.media_type}/${item.id}`}
+                className="hover:bg-[#3A3A3A] duration-300 text-zinc-300 font-semibold w-full p-4 flex items-center border-b border-zinc-600"
+              >
+                <img
+                  src={`https://image.tmdb.org/t/p/w92${
+                    item.poster_path || item.profile_path
+                  }`}
+                  alt={item.name || item.title}
+                  className="w-10 h-14 object-cover mr-3 bg-zinc-600 rounded"
+                />
+                <span>
+                  {item.name ||
+                    item.title ||
+                    item.original_name ||
+                    item.original_title}
+                </span>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
 };
-
 export default Topnav;
