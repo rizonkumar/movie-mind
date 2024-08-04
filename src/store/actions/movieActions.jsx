34d366fa13d2c619021fb8874b3ec092f60fec13
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-08-04 15:09:25 +0530

    Feat: Implemented Movie Actions

--- src/store/actions/movieActions.jsx
+++ src/store/actions/movieActions.jsx
@@ -1,3 +1,27 @@
 export { removeMovie } from "../reducers/movieSlice";
 import axios from "../../utils/axios";
 import { loadMovie } from "../reducers/movieSlice";
+
+export const asyncloadmovie = (id) => async (dispatch, getState) => {
+  try {
+    const detail = await axios.get(`/movie/${id}`);
+    const externalId = await axios.get(`/movie/${id}/external_ids`);
+    const recommendations = await axios.get(`/movie/${id}/recommendations`);
+    const similar = await axios.get(`/movie/${id}/similar`);
+    const videos = await axios.get(`/movie/${id}/videos`);
+    const watchProviders = await axios.get(`/movie/${id}/watch/providers`);
+
+    let theUltimateDetails = {
+      detail: detail.data,
+      externalId: externalId.data,
+      recommendations: recommendations.data.results,
+      similar: similar.data.results,
+      videos: videos.data.results.find((m) => m.type === "Trailer"),
+      watchProviders: watchProviders.data.results.IN,
+    };
+    dispatch(loadMovie(theUltimateDetails));
+    console.log("the ultmiate detais", theUltimateDetails);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
