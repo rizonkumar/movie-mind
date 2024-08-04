82d33ce86bad4f0b3f0db7be7524cc24bfefb354
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-08-04 09:33:48 +0530

    Fix: Imported store

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { store } from "./store/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
