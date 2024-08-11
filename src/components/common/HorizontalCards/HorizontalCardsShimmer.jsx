7ea3ef5fa4a37e5b413ead6dc543c97fba027e93
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2024-08-11 16:33:31 +0530

    Feat: For the recommendation implemented Scroll

--- src/components/common/HorizontalCards/HorizontalCardsShimmer.jsx
+++ src/components/common/HorizontalCards/HorizontalCardsShimmer.jsx
@@ -1,21 +1,20 @@
-import React from "react";
 
 const HorizontalCardsShimmer = () => {
-  return (
-    <div className="w-full h-auto sm:h-[54vh] p-4 sm:p-[44px]">
-      <div className="mb-5">
-        <div className="w-48 h-8 bg-zinc-700 rounded animate-pulse"></div>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 sm:gap-6">
-        {[...Array(13)].map((_, index) => (
-          <div
-            key={index}
-            className="w-full h-[40vh] bg-zinc-700 rounded animate-pulse"
-          ></div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-full h-auto p-4 sm:p-[44px] relative">
+            <div className="mb-5">
+                <div className="w-48 h-8 bg-zinc-700 rounded animate-pulse"></div>
+            </div>
+            <div className="flex overflow-x-hidden">
+                {[...Array(5)].map((_, index) => (
+                    <div
+                        key={index}
+                        className="w-[250px] h-[40vh] flex-shrink-0 mx-2 bg-zinc-700 rounded animate-pulse"
+                    ></div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default HorizontalCardsShimmer;
